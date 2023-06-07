6b52ccccfc098c75fe51d86f61040b16839f128d
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-06-07 02:00:41 -0300

    + Remove beer constants to another file
    + Add expertSystem.py. This file contains the rules for each kind of birra. Is not working due to an error with imports, I give up for today :(

--- server/main.py
+++ server/main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from schema.schema import InputBody, ValidationError
+from expertSystem.expertSystem import getCandidateBeers
 from fastapi.responses import JSONResponse
 from fastapi import status
-
-# Possible beers
-CREAM_ALE = "Cream Ale"
-BALTIC_PORTER = "Baltic Porter"
-KOLSCH = "Kolsch"
-WHITE_IPA = "Ipa Blanca"
-CZECH_AMBER_LAGER = "Lager Ambar Checa"
-NATALIA_NATALIA = "No es posible realizar una birra con estos atributos"
 
 app = FastAPI()
 
@@ -28,9 +21,6 @@ def handle_course_exception(request: Request, exc: ValidationError):
         content=jsonable_encoder({"message": exc.message, "status": exc.status_code}),
     )
 
-def getCandidateBeers(data):
-    # ToDo: implement logic with experta to get the candidate beers
-    return ["Cream Ale", "Sarasa"]
 
 # Run the server using uvicorn
 if __name__ == "__main__":
