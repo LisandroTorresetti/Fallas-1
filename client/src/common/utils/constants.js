c090d01db9205c9eae9e9c3a6f28a9124e700400
FrancoBar <fbarreneche@fi.uba.ar>
2023-06-11 12:56:06 -0300

    Fill questions and header

--- client/src/common/utils/constants.js
+++ client/src/common/utils/constants.js
@@ -1,66 +1,66 @@
 export const INTENSITY_TYPES = {
-  description: "TODO",
+  description: "¿Que intensidad te gustaría?",
   label: "Intensidad",
   idLabel: "intensity",
   values: [
     { value: "baja", name: "Baja" },
     { value: "media", name: "Media" },
     { value: "alta", name: "Alta" },
-  ]
-}
+  ],
+};
 
 export const COLOR_TYPES = {
-  description: "TODO",
+  description: "¿Cuál es la oscuridad que esperás?",
   label: "Oscuridad",
   idLabel: "color",
   values: [
     { value: "palido", name: "Pálido" },
     { value: "ambar", name: "Ámbar" },
     { value: "oscuro", name: "Oscuro" },
-  ]
-}
+  ],
+};
 
 export const BITTERNESS_TYPES = {
-  description: "TODO",
+  description: "¿Cuán amarga deseas que sea?",
   label: "Amargor",
   idLabel: "bitterness",
   values: [
     { value: "bajo", name: "Bajo" },
     { value: "medio", name: "Medio" },
     { value: "alto", name: "Alto" },
-  ]
-}
+  ],
+};
 
 export const HOP_TYPES = {
-  description: "TODO"
-,  label: "Lúpulo",
+  description: "¿De qué categoría de lúpulo disponés?",
+  label: "Lúpulo",
   idLabel: "hop",
   values: [
     { value: "viejo mundo", name: "Viejo mundo" },
     { value: "nuevo mundo", name: "Nuevo mundo" },
-  ]
-}
+  ],
+};
 
 export const FERMENTATION_TYPES = {
-  description: "TODO",
+  description: "¿Qué tipo de fermentación podés garantizar?",
   label: "Fermentación",
   idLabel: "fermentation",
   values: [
     { value: "baja", name: "Baja" },
     { value: "media", name: "Media" },
     { value: "alta", name: "Alta" },
-  ]
-}
+  ],
+};
 
 export const YEAST_TYPES = {
-  description: "TODO",
+  description: "¿Qué levadura podrías utilizar?",
   label: "Levadura",
   idLabel: "yeast",
   values: [
     { value: "lager", name: "Lager" },
     { value: "ale", name: "Ale" },
-  ]
-}
+  ],
+};
 
 export const ALL_TYPES = {
   intensity: INTENSITY_TYPES,
@@ -69,4 +69,4 @@ export const ALL_TYPES = {
   hop: HOP_TYPES,
   fermentation: FERMENTATION_TYPES,
   yeast: YEAST_TYPES,
-}
+};
